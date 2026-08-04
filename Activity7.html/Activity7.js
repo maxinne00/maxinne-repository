45ae78034bae21c701ede4290328878300efaa44
maxinne00 <maxinneamaralit@gmail.com>
2026-08-04 20:24:50 +0800

    final act 7

---
+++ Activity7.html/Activity7.js
@@ -0,0 +1,46 @@
+function changeColor() {
+    document.getElementById("planet").src="colored.png";
+}
+
+function changeColor2() {
+    document.getElementById("planet").src="bw.png";
+}
+
+function changeColor3() {
+    document.getElementById("planet").src="galaxy.png";
+}
+
+function changeColor4() {
+    document.getElementById("planet").src="sun.png";
+}
+
+function changeColor5() {
+    document.getElementById("planet").src="mercury.png";
+}
+
+function changeColor6() {
+    document.getElementById("planet").src="venus.png";
+}
+
+function changeColor7() {
+    document.getElementById("planet").src="earth.png";
+}
+
+function changeColor8() {
+    document.getElementById("planet").src="mars.png";
+}
+
+function changeColor9() {
+    document.getElementById("planet").src="jupiter.png";
+}
+
+function changeColor10() {
+    document.getElementById("planet").src="saturn.png";
+}
+
+function changeColor11() {
+    document.getElementById("planet").src="uranus.png";
+}
+function changeColor12() {
+    document.getElementById("planet").src="neptune.png";
+}
