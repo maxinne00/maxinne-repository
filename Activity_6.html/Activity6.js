9e62a3367abb940c826d76a5157b703882006fb6
maxinne00 <maxinneamaralit@gmail.com>
2026-08-04 18:35:35 +0800

    final

---
+++ Activity_6.html/Activity6.js
@@ -0,0 +1,31 @@
+function changeColor() {
+    document.getElementById("box").src="pink.png";
+}
+
+function changeColor2() {
+    document.getElementById("box").src="blue.png";
+}
+
+function changeColor3() {
+    document.getElementById("box").src="purple.png";
+}
+
+function changeColor4() {
+    document.getElementById("box").src="yellow.png";
+}
+
+function changeColor5() {
+    document.getElementById("box").src="green.png";
+}
+
+function changeColor6() {
+    document.getElementById("box").src="red.png";
+}
+
+function changeColor7() {
+    document.getElementById("box").src="brown.png";
+}
+
+function changeColor8() {
+    document.getElementById("box").src="black.png";
+}
